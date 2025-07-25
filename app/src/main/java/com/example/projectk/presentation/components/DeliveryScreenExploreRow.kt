06409ab1ba1170b52c368084a845ec3d590e80e8
pratap <pratapbantawa3@gmail.com>
2025-07-25 16:55:18 +0545

    DeliveryScreenSearch

--- app/src/main/java/com/example/projectk/presentation/components/DeliveryScreenExploreRow.kt
+++ app/src/main/java/com/example/projectk/presentation/components/DeliveryScreenExploreRow.kt
@@ -24,7 +24,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.projectk.R  // Replace with your actual package name
+import com.example.projectk.R
 
 
 @Composable
